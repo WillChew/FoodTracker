66efc911d5374ff1c40677720b251a1610582a05
WillChew <williamchew95@gmail.com>
2018-07-16 14:35:00 -0400

    Initial Commit

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,105 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 11/10/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+
+class Meal: NSObject, NSCoding {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    var desc: String
+    var calories: Int
+    var id: Int?
+    
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+        static let desc = "description"
+        static let calories = "calories"
+        static let id = "id"
+    }
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int, desc: String, calories: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        self.desc = desc
+        self.calories = calories
+        
+    }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+        aCoder.encode(desc, forKey: PropertyKey.desc)
+        aCoder.encode(calories, forKey: PropertyKey.calories)
+        aCoder.encode(id, forKey: PropertyKey.id)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        guard let desc = aDecoder.decodeObject(forKey: PropertyKey.desc) as? String else {
+            os_log("Unable to recde the descrition for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let calories = aDecoder.decodeInteger(forKey: PropertyKey.calories)
+        
+        let id = aDecoder.decodeInteger(forKey: PropertyKey.id)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating, desc: desc, calories: calories)
+        
+    }
+}
---
+++ FoodTracker/RequestManager.swift
@@ -0,0 +1,48 @@
+//
+//  RequestManager.swift
+//  FoodTracker
+//
+//  Created by Will Chew on 2018-07-16.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+
+
+class RequestManager {
+    var meal: Meal!
+    
+    
+    func sendRequest(_ meal: Meal) {
+        
+//        let parameters: [String : Any] = ["title" : meal.name, "description": meal.desc, "calories": meal.calories]
+        
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let url = URL(string: "https://cloud-tracker.herokuapp.com")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+
+
+        components.path = "/users/me/meals"
+        
+        let titleQueryItem = NSURLQueryItem(name: "title", value: meal.name)
+        let descriptionQueryItem = NSURLQueryItem(name: "description", value: meal.desc)
+        let caloriesQueryItem = NSURLQueryItem(name: "calories", value: String(meal.calories))
+        components.queryItems = [titleQueryItem, descriptionQueryItem, caloriesQueryItem] as [URLQueryItem]
+        print(#line, components.url ?? "No url")
+        let request = NSMutableURLRequest(url: components.url! )
+
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("mzLeVQvHeMyoJy71stmYK99A", forHTTPHeaderField: "token")
+        request.httpMethod = "POST"
+        
+       
+        
+        
+        
+        
+    }
+}
